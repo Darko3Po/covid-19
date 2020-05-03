a6dc494750a8b90060e734ce911f5fcdd7c53f87
Darko3Po <49729584+Darko3Po@users.noreply.github.com>
2020-05-03 16:06:50 +0200

    Add files via upload

---
+++ js/js-quiz-questions.js
@@ -0,0 +1,45 @@
+/* [THE "DATABASE" - QUESTIONS, OPTIONS, ANSWERS] */
+// An array that contains objects
+// In the format of {q: QUESTION, o: OPTIONS, a: CORRECT ANSWER}
+var questions = [
+  {
+    q : "How old are you?",
+    o : [
+      "<30 years",
+      ">30-60< years",
+      ">60 years"    ],
+    a : 2 // arrays start with 0, so it is 70 meters
+  },
+  {
+    q : "Have you had cancer?",
+    o : [
+      "YES",
+      "NO"
+    ],
+    a : 0
+  },
+  {
+    q : "Do you have heart or diabetes problems?",
+    o : [
+      "YES",
+      "NO"
+    ],
+    a : 0
+  },
+  {
+    q : "Do you have problems with your lungs and breathing?",
+    o : [
+      "YES",
+      "NO"
+    ],
+    a : 0
+  },
+  {
+    q : "Do you have any chronic form of the disease?",
+    o : [
+      "YES",
+      "NO"
+    ],
+    a : 0
+  }
+];
